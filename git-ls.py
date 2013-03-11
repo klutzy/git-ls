09b1930f272ae41ff3a262df7c512e85dd2d731b
klutzy <klutzytheklutzy@gmail.com>
2013-03-11 18:02:22 +0900

    Print file list using git-ls-tree

--- git-ls.py
+++ git-ls.py
@@ -42,12 +42,33 @@ def git_status():
     return ret
 
 
-def output_line(x, y, path, path_to):
+def git_ls_tree(path, tree="HEAD"):
+    ret = []
+
+    path = os.path.normpath(path) + "/"
+    output = git("ls-tree", "--full-name", tree, path)
+    for line in output.splitlines():
+        tmp, file_name = line.split("\t", 1)
+        file_mode, file_type, file_obj = tmp.split(" ")
+        # TODO file_size?
+        ret.append((file_mode, file_type, file_obj, file_name))
+
+    return ret
+
+
+def output_line(x, y, path, path_to=None, path_from=None,
+                with_untracked=False):
     output = path
     extra = ""
 
     if path_to:
         output = "{path} -> {path_to}".format(path=path, path_to=path_to)
+    elif path_from:
+        output = "{path} <- {path_from}".format(path=path,
+                                                path_from=path_from)
+
+    if with_untracked:
+        extra += c("*", bold=True)
 
     # TODO use `git config color.status.???`
     color = 0
@@ -75,9 +96,38 @@ def main():
         print("not inside working directory")
         return
 
-    for x, y, path, path_to in git_status():
-        print output_line(x, y, path, path_to)
-    pass
+    prefix = git("rev-parse", "--show-prefix").strip()  # can be ""
+
+    status = git_status()
+    ls_tree = git_ls_tree(prefix)
+
+    done = []
+
+    # print working tree status
+    for file_mod, file_type, file_obj, file_name in ls_tree:
+        x, y, path_from, path_to = '', '', None, None
+        with_untracked = False
+        if file_type == 'blob':
+            file_status = [i for i in status if file_name in i[2:]]
+            # len(file_status) can be >1 e.g. `git rm file --cached`
+            for info in file_status:
+                x, y, pf, pt = info
+                if (x, y) == ("?", "?"):
+                    with_untracked = True
+                elif pt == file_name:
+                    path_from = pf
+                elif pf == file_name:
+                    path_to = pt
+        print output_line(x, y, file_name, path_from=path_from,
+                          path_to=path_to, with_untracked=with_untracked)
+        done.append(file_name)
+
+    # print new and untracked files
+    for x, y, file_name, _ in status:
+        if file_name in done:
+            continue
+        print output_line(x, y, file_name)
+        done.append(file_name)
 
 
 if __name__ == '__main__':
